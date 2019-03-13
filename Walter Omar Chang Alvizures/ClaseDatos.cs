b4eaef7662592afff311651f9338375b36a21956
ingwchang <48505723+ingwchang@users.noreply.github.com>
2019-03-12 23:44:28 -0600

    Add files via upload

---
+++ Walter Omar Chang Alvizures/ClaseDatos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walter_Omar_Chang_Alvizures
+{
+    class ClaseDatos
+    {
+        public string NomFig { get; set; }
+        public double bTriangulo { get; set; }//Base del Triangulo  
+        public double aTriangulo { get; set; }//Altura del Triangulo
+        public double ArTriangulo { get; set; }//Area del Triangulo
+        public double bRectangulo { get; set; }//Base del Rectangulo
+        public double aRectangulo { get; set; }//Altura del Rectangulo
+        public double ArRectangulo { get; set; }//Area del Rectangulo
+        public double RadioCir { get; set; }//Radio del Circulo
+        public double AreaCir { get; set; }//Area del Circulo
+
+        public virtual void DatosFiguras()
+        {
+            Console.WriteLine("MOSTRAR LOS DATOS");
+        }
+    }
+
+    class Triangulo : ClaseDatos
+    {
+        public override void DatosFiguras()
+        {
+            Console.WriteLine();
+            Console.WriteLine("INGRESE EL NOMBRE DE LA FIGURA");
+            base.NomFig = Console.ReadLine();
+            Console.WriteLine("INGRESE LOS VALORES DEL RADIO:");
+            base.bTriangulo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INGRESE LOS VALORES DE LA ALTURA:");
+            base.aTriangulo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INGRESE LOS VALORES DEL AREA:");
+            base.ArTriangulo = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    class Rectangulo : ClaseDatos
+    {
+        public override void DatosFiguras()
+        {
+            Console.WriteLine();
+            Console.WriteLine("INGRESE EL NOMBRE DE LA FIGURA");
+            base.NomFig = Console.ReadLine();
+            Console.WriteLine("INGRESE LOS VALORES DEL RADIO:");
+            base.bRectangulo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INGRESE LOS VALORES DE LA ALTURA:");
+            base.aRectangulo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INGRESE LOS VALORES DEL AREA:");
+            base.ArRectangulo = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    class Circulo : ClaseDatos
+    {
+        public override void DatosFiguras()
+        {
+            Console.WriteLine();
+            Console.WriteLine("INGRESE EL NOMBRE DE LA FIGURA");
+            base.NomFig = Console.ReadLine();
+            Console.WriteLine("INGRESE LOS VALORES DEL RADIO:");
+            base.RadioCir = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INGRESE LOS VALORES DEL AREA:");
+            base.AreaCir = Convert.ToInt16(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Walter Omar Chang Alvizures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walter_Omar_Chang_Alvizures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Figura1 = new Triangulo();  //Figura1 es Triangulo.
+            Figura1.DatosFiguras();
+            Console.WriteLine();
+            Console.WriteLine("NOMBRE DE LA FIGURA:{0}", Figura1.NomFig);
+            Console.WriteLine("EL TAMAÑO DE LA BASE DEL {0} ES DE:{1}",Figura1.NomFig , Figura1.bTriangulo);
+            Console.WriteLine("LA ALTURA DEL {0} ES DE:{1}",Figura1.NomFig , Figura1.aTriangulo);
+            Console.WriteLine("EL TAMAÑO DEL AREA DE {0} ES DE:{1}",Figura1.NomFig , Figura1.ArTriangulo);
+
+            var Figura2 = new Rectangulo(); //Figura2 es Rectangulo.
+            Figura2.DatosFiguras();
+            Console.WriteLine();
+            Console.WriteLine("NOMBRE DE LA FIGURA:{0}", Figura2.NomFig);
+            Console.WriteLine("EL TAMAÑO DE LA BASE DEL {0} ES DE:{1}",Figura2.NomFig , Figura2.bRectangulo);
+            Console.WriteLine("LA ALTURA DEL {0} ES DE:{1}",Figura2.NomFig , Figura2.aRectangulo);
+            Console.WriteLine("EL TAMAÑO DEL AREA DE {0} ES DE:{1}",Figura2.NomFig , Figura2.ArRectangulo);
+
+            var Figura3 = new Circulo(); //Figura3 es Circulo.
+            Figura3.DatosFiguras();
+            Console.WriteLine();
+            Console.WriteLine("NOMBRE DE LA FIGURA:{0}", Figura3.NomFig);
+            Console.WriteLine("EL TAMAÑO DEL RADIO DEL {0} ES DE:{1}",Figura3.NomFig , Figura3.RadioCir);
+            Console.WriteLine("EL TAMAÑO DEL AREA DEL {0} ES DE:{1}",Figura3.NomFig , Figura3.AreaCir);
+            Console.ReadKey();
+        }
+    }
+}
